1cf9df212f1ffe2fdc1c67e679f4ae125ca8e7bb
hamdyyemadd <hamdyyemad@aol.com>
2023-12-06 01:18:25 +0200

    adding cookieParser
    adding cookieParser

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require("cors")
-const config = require('./config')
-const router = require('./routers/index')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./config");
+const router = require("./routers/index");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+const app = express();
+mongoose
+  .connect(config.mongo_url)
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
 
-const app = express()
-mongoose.connect(config.mongo_url).then(() => console.log("database connected")).catch(err => console.log(err))
-
-app.use(cors())
-app.use(express.json())
-app.use(router)
-app.get('/', (req, res) => {
-    res.send("hello server")
-})
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.get("/", (req, res) => {
+  res.send("hello server");
+});
 
 app.listen(config.port, () => {
-    console.log("server is started at port " + config.port)
-})
+  console.log("server is started at port " + config.port);
+});
