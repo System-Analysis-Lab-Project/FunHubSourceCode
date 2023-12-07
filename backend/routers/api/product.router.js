2afc0938f9453845580b36a4f8e236c310053b67
ahmedtaha-ux <ahmedtaha9792@gmail.com>
2023-12-07 23:06:29 +0200

    update product model

--- backend/routers/api/product.router.js
+++ backend/routers/api/product.router.js
@@ -3,6 +3,7 @@ const Router = require('express').Router
 const router = Router()
 
 router.get('/:_id', productController.get_product_by_id)
+router.get('/', productController.get_all_products)
 router.post('/', productController.add_product)
 router.put('/:_id', productController.update_product)
 router.delete('/:_id', productController.delete_product)
