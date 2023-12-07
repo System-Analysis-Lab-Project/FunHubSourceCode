3ced2dc45e96d2a3a0ea1fddb4c8efb2272a2082
ahmedtaha-ux <ahmedtaha9792@gmail.com>
2023-12-07 17:27:52 +0200

    Add Product model, controller, router

---
+++ backend/routers/api/product.router.js
@@ -0,0 +1,9 @@
+const productController = require('../../controllers/product.controller')
+const Router = require('express').Router
+const router = Router()
+
+router.get('/:_id', productController.get_product_by_id)
+router.post('/', productController.add_product)
+router.put('/:_id', productController.update_product)
+router.delete('/:_id', productController.delete_product)
+module.exports = router
--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router
 const router = Router()
 const userRouter = require('./api/user.router')
+const productRouter = require('./api/product.router')
 
 router.use('/user', userRouter)
+router.use('/product', productRouter)
 
 module.exports = router
