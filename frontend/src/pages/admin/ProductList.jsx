c027ddfda2166145baaec36d769d5b981565a9f7
hamdyyemadd <hamdyyemad@aol.com>
2023-12-09 01:55:28 +0200

    changing styles & adding refresh button
    changing styles & adding refresh button

--- frontend/src/pages/admin/ProductList.jsx
+++ frontend/src/pages/admin/ProductList.jsx
@@ -72,61 +72,43 @@ export default function ProductList() {
       ) : (
         <>
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-start items-end flex-col">
-            <button
-              type="button"
-              onClick={createProductHandler}
-              className=" m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Add Product
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+            <div className="flex ">
+              <button
+                onClick={() => window.location.reload()}
+                className="flex items-center m-2 text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
               >
-                <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </button>
-
-            {/* <div className="p-4">
-            <label htmlFor="table-search" className="sr-only">
-              Search
-            </label>
-            <div className="relative mt-1">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
+                  className="w-5 h-5 mx-1"
                   xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
                 >
-                  <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
+                  <path d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" />
                 </svg>
-              </div>
-              <input
-                type="text"
-                id="table-search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search for items"
-              />
+
+                <span className="mx-1">Refresh</span>
+              </button>
+              <button
+                type="button"
+                onClick={createProductHandler}
+                className=" m-2 text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+              >
+                Add Product
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path stroke="currentColor" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+              </button>
             </div>
-          </div> */}
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+
+            <table className="w-full text-sm text-left text-gray-400">
+              <thead className="text-xs  uppercase bg-gray-700 text-gray-400">
                 <tr>
-                  {/* <th scope="col" className="p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-all-search"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label htmlFor="checkbox-all-search" className="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </th> */}
                   <th scope="col" className="px-6 py-3">
                     ID
                   </th>
@@ -151,30 +133,15 @@ export default function ProductList() {
                 </tr>
               </thead>
               <tbody>
-                {/* <td className="w-4 p-4">
-                  <div className="flex items-center">
-                  <input
-                  id="checkbox-table-search-1"
-                  type="checkbox"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label
-                  htmlFor="checkbox-table-search-1"
-                  className="sr-only"
-                    >
-                      checkbox
-                    </label>
-                    </div>
-                </td> */}
                 {product.map((p) => (
                   <tr
                     key={p._id}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    className=" border-b bg-gray-800 border-gray-700 hover:bg-gray-50 hover:bg-gray-600"
                   >
                     <td className="px-6 py-4">{p._id}</td>
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                      className="px-6 py-4 font-medium text-white whitespace-nowrap"
                     >
                       {p.name}
                     </th>
@@ -182,14 +149,14 @@ export default function ProductList() {
                     <td className="px-6 py-4">{p.category}</td>
                     <td className="px-6 py-4">${p.price}</td>
                     <td className="px-6 py-4 text-right">
-                      <a className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                      <a className="font-medium text-blue-500 hover:underline">
                         Edit
                       </a>
                     </td>
                     <td className="px-6 py-4 text-right">
                       <button
                         onClick={() => deleteProductHandler(p._id)}
-                        className="pointer font-medium text-red-600 dark:text-blue-red hover:underline"
+                        className="pointer font-medium text-red-400 hover:underline"
                       >
                         Delete
                       </button>
